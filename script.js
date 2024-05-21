6190cd5f0a85fd59b28f4b334be766fb23b6da09
Piyush Sharma <134504369+Piyush2321@users.noreply.github.com>
2024-05-21 11:40:20 +0530

    Update api-key in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "99a62a5f5fe24bdf8357c565960d3f90";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
